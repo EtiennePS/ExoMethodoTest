fad57033d30450ed5902137b244438579124360e
EtiennePS <etienne.picherit@gmail.com>
2021-04-09 16:24:13 +0200

    Initialisation du projet

---
+++ src/main/java/com/ynov/methotest1/Methotest1Application.java
@@ -0,0 +1,13 @@
+package com.ynov.methotest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Methotest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Methotest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/methotest1/Methotest1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ynov.methotest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Methotest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	
+
+}
